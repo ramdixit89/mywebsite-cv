14123fd33ed5090211808dfdc7c88b2f6c8861fd
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2025-07-02 09:50:14 +0530

    code added

--- script.js
+++ script.js
@@ -1,79 +1,76 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    // Sticky navbar
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
+
+        // Scroll-up button
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+    // Scroll to top
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 }, 'slow');
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+    // Smooth scroll on menu click
+    $('.navbar .menu li a').click(function () {
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    // Toggle navbar
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["FullStack Developer"],
-        typeSpeed: 60,
-        backSpeed: 40,
-        loop: true
-    });
-    var typed = new Typed(".typing-3", {
-        strings: ["Connect with me on :)"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // Typed.js animations
+    if ($(".typing").length) {
+        new Typed(".typing", {
+            strings: ["FullStack Developer"],
+            typeSpeed: 60,
+            backSpeed: 40,
+            loop: true
+        });
+    }
 
-    var typed = new Typed(".typing-2", {
-        strings: ["FullStack Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    if ($(".typing-2").length) {
+        new Typed(".typing-2", {
+            strings: ["FullStack Developer"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+    }
 
-    // owl carousel script
+    if ($(".typing-3").length) {
+        new Typed(".typing-3", {
+            strings: ["Connect with me on :)"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+    }
+
+    // Owl Carousel
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
+            0: { items: 1, nav: false },
+            600: { items: 2, nav: false },
+            1000: { items: 3, nav: false }
         }
     });
 });
